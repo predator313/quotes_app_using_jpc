faf06718c04397dc7d626c077f9def864dc1d700
Android Dev <aamirnasa97@gmail.com>
2023-09-16 23:36:29 +0530

    card view in compose

--- app/src/main/java/com/aamirashraf/quotesappusingjpc/screen/QuotesListItem.kt
+++ app/src/main/java/com/aamirashraf/quotesappusingjpc/screen/QuotesListItem.kt
@@ -3,10 +3,12 @@ package com.aamirashraf.quotesappusingjpc.screen
 import android.graphics.ColorFilter
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
@@ -14,20 +16,23 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Close
 import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.FormatQuote
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Alignment.Companion.Center
+import androidx.compose.ui.Alignment.Companion.CenterHorizontally
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.rotate
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
-@Preview
 @Composable
 fun quotesItem(){
     Card(
@@ -40,7 +45,7 @@ fun quotesItem(){
             modifier = Modifier.padding(16.dp)
         ) {
             Image(
-                imageVector = Icons.Filled.Favorite,
+                imageVector = Icons.Filled.FormatQuote,
                 colorFilter = androidx.compose.ui.graphics.ColorFilter.tint(Color.White),
                 alignment = Alignment.TopStart,
                 contentDescription = "Quotes",
@@ -55,7 +60,7 @@ fun quotesItem(){
                 style = MaterialTheme.typography.bodySmall,
                 modifier = Modifier.padding(0.dp,0.dp,0.dp,8.dp))
                 Box(modifier = Modifier
-                    .background(Color.Black)
+                    .background(Color(0xFFEEEEEE))
                     .fillMaxWidth(0.4f)
                     .height(1.dp)
                 )
@@ -63,6 +68,56 @@ fun quotesItem(){
                     style = MaterialTheme.typography.bodySmall,
                     fontWeight = FontWeight.Thin,
                     modifier = Modifier.padding(top = 4.dp)
+                )
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+fun quotesDetails(){
+    Box(contentAlignment = Alignment.Center,
+        modifier = Modifier
+            .fillMaxSize(1f)
+            .background(
+                Brush.sweepGradient(
+                    colors = listOf(
+                        Color(0xffffffff),
+                        Color(0xffe3e3e3)
+                    )
+                )
+            )
+    ){
+        Card(
+            elevation = CardDefaults.cardElevation(
+                defaultElevation = 4.dp
+            ),
+            modifier = Modifier.padding(32.dp)
+        ){
+            Column(
+                verticalArrangement = Arrangement.Center,
+
+                modifier = Modifier
+
+                    
+                    .padding(16.dp,24.dp)
+            ) {
+                Image(
+                    imageVector = Icons.Filled.FormatQuote,
+                    contentDescription = "Quotes",
+                    modifier= Modifier
+                        .size(80.dp)
+                        .rotate(180f)
+                )
+                Text(text = "Time is the most valuable thing a man can spend",
+                    style = MaterialTheme.typography.bodySmall,
+
+                )
+                Spacer(modifier = Modifier.height(16.dp))
+                Text(text = "Theopharatus",
+                    style = MaterialTheme.typography.bodySmall,
+                    
                 )
             }
         }
